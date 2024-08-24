1f666adcf8e89c4f1f106418d6d83392965c2cd4
ShwetaPaygude <shwetapaygude2325@gmail.com>
2024-08-24 19:43:01 +0530

    content chnages

--- src/Desktop/Body/Content.jsx
+++ src/Desktop/Body/Content.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Content() {
+
+ const Content =()=>{
+    const[isModalOpen,setIsModalOpen]=useState(false);
+    const [groupNames, setGroupNames] = useState(
+        JSON.parse(localStorage.getItem("groupNames")) || {}
+      );
+    const[selectedGroupname,setSelectedGroupName]=useState("");
+    const[messages,setMessages]=useState(
+        JSON.parse(localStorage.getItem("messages"))||[]
+    );
+
+ }
   return (
     <div>
       add new this and see the changes
