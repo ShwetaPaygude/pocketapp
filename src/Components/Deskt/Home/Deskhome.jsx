45797bc5fa14ccca36c015ae58d9d9c62cf83284
ShwetaPaygude <shwetapaygude2325@gmail.com>
2024-08-28 22:12:33 +0530

    homepage

--- src/Components/Deskt/Home/Deskhome.jsx
+++ src/Components/Deskt/Home/Deskhome.jsx
@@ -1,16 +1,44 @@
 import React from 'react'
-import logo from "../../../assets/logo.png";
+import logo from "../../../assets/main.png";
 import lock from "../../../assets/lock.png";
-import "../Desk.module.css";
+import "../Deskstyle.css";
 
 
 function Deskhome() {
   return (
-    <div>
-      {logo}
-      {lock}
-    </div>
-  )
+    <div className="desk_home">
+			<LogoSection />
+			<Description />
+			<EncryptionMessage />
+		</div>
+	);
 }
+function LogoSection() {
+	return (
+		<>
+			<img src={logo} alt="some folks with notepad and pencils" />
+			<h1>Pocket Notes</h1>
+		</>
+	);
+}
+function Description() {
+	return (
+		<p>
+			Send and receive messages without keeping your phone online.
+			<br />
+			Use Pocket Notes on up to 4 linked devices and 1 mobile phone.
+		</p>
+	);
+}
+
+function EncryptionMessage() {
+	return (
+		<div className="encryption-msg">
+			<img src={lock} alt="lock" />
+			<span> end-to-end encrypted</span>
+		</div>
+	);
+}
+
 
 export default Deskhome
