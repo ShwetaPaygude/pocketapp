dc0f7158adf1f1c7a3f0afb7af8bd45924506d84
ShwetaPaygude <shwetapaygude2325@gmail.com>
2024-08-28 22:14:41 +0530

    left side content

--- src/Components/Deskt/Sidepannel.jsx
+++ src/Components/Deskt/Sidepannel.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Deskstyle.css";
+import Dmodal from "./Dmodal";
+import Group from "./Group";
+const Sidepannel = ({ selected, setSelected }) => {
+	const [titles, setTitles] = useState([]);
+	const [showPrompt, setShowPrompt] = useState(false);
+	const [groupNamesParent, setGroupNamesParent] = useState(
+		localStorage.getItem("groupNames") || []
+	);
 
-function Sidepannel() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	useEffect(() => {
+		const data = localStorage.getItem("groupNames");
+		if (data) {
+			setGroupNamesParent(JSON.parse(data));
+		} else {
+			setGroupNamesParent([]);
+		}
+	}, []);
 
-export default Sidepannel
+	useEffect(() => {
+		if (groupNamesParent.length > 0) {
+			const obj = JSON.parse(localStorage.getItem("groupNames"));
+			const result = Object.keys(obj).map((key) => [obj[key]]);
+			setTitles(result);
+		}
+	}, [groupNamesParent]);
+
+	const handleClick = () => {
+		setShowPrompt(true);
+	};
+
+	const handleClose = () => {
+		setShowPrompt(false);
+	};
+
+	return (
+		<div className="desk_sidebar">
+			<div className="desk_sidebar_heading">Pocket Notes</div>
+			<div className="desk_sidebar_btn">
+				<button onClick={handleClick}>
+					<span id="add">+</span>
+					
+				</button>
+			</div>
+			<div className="desk_sidebar_notes_heading">
+				{titles.length > 0 ? (
+					titles.map((title, index) => (
+						<Group
+							selected={selected}
+							setSelected={setSelected}
+							key={index}
+							title={title}
+						/>
+					))
+				) : (
+					<div className="desk_sidebar_notes_title_empty"></div>
+				)}
+			</div>
+			{showPrompt && (
+				<div className="desk_prompt_overlay">
+					<Dmodal
+						groupNamesParent={groupNamesParent}
+						setGroupNamesParent={setGroupNamesParent}
+						onClose={handleClose}
+					/>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Sidepannel;
