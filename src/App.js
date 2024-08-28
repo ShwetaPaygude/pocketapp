3c52fdd193c44ed944dbf30078ff872e121bb32d
ShwetaPaygude <shwetapaygude2325@gmail.com>
2024-08-28 22:16:31 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import { useState, useEffect, useCallback } from "react";
 import "./App.css";
+import Dscreen from "./Screen/Desktop/Dscreen"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+function App() {
 
+  return (
+    <div className="App">
+      
+        <Dscreen />
+     
+       
+    </div>
+  );
+}
 
 export default App;
